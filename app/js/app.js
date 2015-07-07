88ee5a3539c882fc0e5710dfa82663aa15cdf0e9
Peter McMillan <peterm95018@gmail.com>
2015-07-07 08:22:34 -0700

    updated files up to touch navigation in tutorial from https://masteringmean.com/lessons/434?li=1

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,25 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'ngTouch'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeCtrl'
+    });
+    $routeProvider.when('/add-expense', {
+      templateUrl: 'partials/add-expense.html',
+      controller: 'AddExpenseCtrl'
+    });
+    $routeProvider.when('/view-summary', {
+      templateUrl: 'partials/view-summary.html',
+      controller: 'ViewSummaryCtrl'
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+  }
+]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,50 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('HomeCtrl', ['$scope', 'categoryList',
+  	function($scope, categoryList) {
+  		$scope.categories = categoryList;
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('AddExpenseCtrl', ['$scope', 'categoryList', 'expService',
+  	function($scope, categoryList, expService) {
+  		$scope.categories = categoryList;
 
-  }]);
+  		$scope.submit = function() {
+  			expService.saveExpense($scope.expense);
+  		};
+  }])
+  .controller('ViewSummaryCtrl', ['$scope', 'categoryList', 'expService',
+  	function($scope, categoryList, expService) {
+  		$scope.expenses = expService.getExpense();
+
+      $scope.summaryData = [];
+
+      var categories = categoryList;
+      
+      categories.forEach(function(item) {
+        var catTotal = expService.getCategoryTotal(item);
+
+        $scope.summaryData.push({
+            category: item,
+            amount: catTotal
+        });
+
+    });
+  	
+  }])
+.controller('NavigationCtrl',['$scope' ,'$location',function($scope,$location){
+  var navigator = function(incrementer) {
+    var pages = ['/', '/add-expense', '/view-summary'];
+
+    var nextUrl = "";
+    var currentPage = $location.path();
+    var lastPageIndex = pages.length - 1;
+    var pageIndex = pages.indexOf(currentPage);
+    var direction = pageIndex + incrementer;
+    if (direction === -1) direction = lastPageIndex;
+    if (direction > lastPageIndex) direction = 0;
+    nextUrl = pages[direction];
+    $location.url(nextUrl);
+  };
+}]);
